af0efe810d4139f4f1aceb56d2248502197d72da
Kristel <kristel.kvillanueva@gmail.com>
2019-10-23 16:12:50 +1300

    Finalise Unit test using mockito

---
+++ src/main/java/com/A3_PartA/_IT7320_Assignment3_PartA/CalcSalaryCompute.java
@@ -0,0 +1,66 @@
+package com.A3_PartA._IT7320_Assignment3_PartA;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalcSalaryCompute {
+	
+	Employee emp = new Employee();
+
+	public double computeTax(double income) {
+		
+		System.out.println("Income from CalcSalary: " + income);
+		
+		double tax = 0.0; 
+		
+		if(income >= 0 && income <=14000) {
+			tax = 10.5; 
+		}else if(income >= 14001 && income <= 48000){
+			tax = 17.5;	
+		}else if(income >= 48001 && income <= 70000){
+			tax = 30.0;	
+		}else if(income > 70000){
+			tax = 33.0;	
+		}
+			
+		System.out.println("Tax from CalcSalary: " + tax);
+		return tax;
+	}
+
+	public double weeklySalary(Employee income) {
+	
+		double weeklySalary = 0; 
+		
+		weeklySalary = (income.getAnnualSalary() - (income.getAnnualSalary()* (income.getTax()/100))) / 52;
+		
+		return weeklySalary;
+	}
+
+	public double fortnightSalary(Employee income) {
+		
+		double fornightSalary = 0; 
+		
+		fornightSalary = income.getWeeklySalary() * 2;
+	
+		return fornightSalary;
+	}
+
+	/* Calculate KiwiSaver */
+	public double computeKiwiSaver(Employee income) {
+		
+		double kiwiSaver = 0.0;
+		
+		Map<Double, Double> kiwiSaverMap = new HashMap<Double, Double>();
+		
+		kiwiSaverMap.put(3.0, 3.0);
+		kiwiSaverMap.put(4.0, 4.0);
+		kiwiSaverMap.put(6.0, 6.0);
+		kiwiSaverMap.put(8.0, 8.0);
+		kiwiSaverMap.put(10.0, 10.0);
+		
+		kiwiSaver = kiwiSaverMap.get(income.getKiwiSaver());
+		kiwiSaver = income.getAnnualSalary() * (kiwiSaver / 100);
+
+		return kiwiSaver;
+	}
+}
--- src/test/java/com/A3_PartA/_IT7320_Assignment3_PartA/CalcSalaryTest.java
+++ src/test/java/com/A3_PartA/_IT7320_Assignment3_PartA/CalcSalaryTest.java
@@ -7,75 +7,83 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-
 public class CalcSalaryTest {
-	
+
 	CalcSalary calcSalaryObj;
-	Employee empObj; 
-	double income;
+	CalcSalaryCompute calcSalaryComputeObj;
+	Employee empObj;
+	double income, kiwiSaverPer;
 	double expectedTax, expectedWeeklySalary, expectedFortnightSalary, expectedKiwiSaver;
 	double actualTax, actualWeeklySalary, actualFortnightSalary, actualKiwiSaver;
-	
-	ISalary mockObj; 
+
+	ISalary mockObj;
 
 	@Before
 	public void setUp() throws Exception {
 		calcSalaryObj = new CalcSalary();
+		calcSalaryComputeObj = new CalcSalaryCompute();
 		empObj = new Employee();
-		income = 110000.00; 
+		income = 110000.00;
 		expectedTax = 33.0;
-		expectedWeeklySalary = (income - (income * (expectedTax/100))) / 52;
+		kiwiSaverPer = 3.0;
+		expectedWeeklySalary = (income - (income * (expectedTax / 100))) / 52;
 		expectedFortnightSalary = expectedWeeklySalary * 2;
-		
+		expectedKiwiSaver = income * (kiwiSaverPer / 100);
+
 		/* Compute Tax */
 		mockObj = Mockito.mock(ISalary.class);
-		Mockito.when(mockObj.computeTax(income)).thenReturn(income);
+		Mockito.when(mockObj.computeTax(income)).thenReturn(calcSalaryComputeObj.computeTax(income));
 		calcSalaryObj.setObj(mockObj);
-		
-		System.out.println("Income from CalcSalaryTest: " + income);
+
 		actualTax = calcSalaryObj.computeTax(income);
+
+		empObj = new Employee(income, actualTax, 0.00, 0.00, kiwiSaverPer);
+
 		
-		empObj = new Employee(income, actualTax, 0.00, 0.00, 0.00);
-		
-		/* Compute actualWeeklySalary */
-		Mockito.when(mockObj.weeklySalary(empObj)).thenReturn(empObj.getWeeklySalary());
-		calcSalaryObj.setObj(mockObj);
-	
-		actualWeeklySalary = calcSalaryObj.weeklySalary(empObj);
-		
-		empObj = new Employee(income, actualTax, actualWeeklySalary, 0.00, 0.00);
-		
-		/* Compute actualForthnightSalary */
-		Mockito.when(mockObj.weeklySalary(empObj)).thenReturn(empObj.getFortnightSalary());
-		calcSalaryObj.setObj(mockObj);
-	
-		actualFortnightSalary = calcSalaryObj.weeklySalary(empObj);
+		  //Compute actualWeeklySalary
+		  Mockito.when(mockObj.weeklySalary(empObj)).thenReturn(calcSalaryComputeObj.weeklySalary(empObj)); 
+		  calcSalaryObj.setObj(mockObj);
+		  
+		  actualWeeklySalary = calcSalaryObj.weeklySalary(empObj);
+		  
+		  empObj = new Employee(income, actualTax, actualWeeklySalary, 0.00,
+		  kiwiSaverPer);
+		  
+		  //Compute actualForthnightSalary
+		  Mockito.when(mockObj.fortnightSalary(empObj)).thenReturn(calcSalaryComputeObj.fortnightSalary(empObj)); 
+		  calcSalaryObj.setObj(mockObj);
+		  
+		  actualFortnightSalary = calcSalaryObj.fortnightSalary(empObj);
+		  
+		  //Compute actualKiwiSaver
+		  Mockito.when(mockObj.computeKiwiSaver(empObj)).thenReturn(calcSalaryComputeObj.computeKiwiSaver(empObj)); 
+		  calcSalaryObj.setObj(mockObj);
+		  
+		  actualKiwiSaver = calcSalaryObj.computeKiwiSaver(empObj);	 
+
 	}
 
-
 	@Test
-	public void testCalcSalary() {
+	public void testComputeTax() {
 		assertEquals(expectedTax, actualTax, 0);
-		
+
 	}
-		
-	@Test
-	public void testWeeklySalary() {
-		assertEquals(expectedWeeklySalary, actualWeeklySalary, 0);
-	}
+
 	
-	@Test
-	public void testFortnightSalary() {
-		assertEquals(expectedFortnightSalary, actualFortnightSalary, 0);
-	}
-	
+	  @Test public void testWeeklySalary() { assertEquals(expectedWeeklySalary,
+	  actualWeeklySalary, 0); }
+	  
+	  @Test public void testFortnightSalary() {
+	  assertEquals(expectedFortnightSalary, actualFortnightSalary, 0); }
+	  
+	  @Test public void testComputeKiwiSaver() { assertEquals(expectedKiwiSaver,
+	  actualKiwiSaver, 0); }
+	 
+
 	@After
 	public void tearDown() throws Exception {
 		calcSalaryObj = null;
 		empObj = null;
 	}
-	
-
-
 
 }
